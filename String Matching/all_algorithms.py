48b046108f6b4b7e21b23de3baa066933814d3b4
Omkar-Shirwadkar <omi.shirwadkar@gmail.com>
2024-06-19 00:55:08 +0530

    KMP

--- String Matching/all_algorithms.py
+++ String Matching/all_algorithms.py
@@ -11,5 +11,46 @@ def naive_approach(pattern, text):
     return ans
 
 
+def KMP(pat, txt):
+    M = len(pat)
+    N = len(txt)
+    lps = computeLPSArray(pat, M)
+    print("LPS array of pattern: ", lps)
+    ans = []
+    i = 0
+    j = 0
+    while (N - i) >= (M - j):
+        if pat[j] == txt[i]:
+            i += 1
+            j += 1
+ 
+        if j == M:
+            ans.append(i-j)
+            j = lps[j-1]
+        elif i < N and pat[j] != txt[i]:
+            if j != 0:
+                j = lps[j-1]
+            else:
+                i += 1
+    return ans
+def computeLPSArray(pat, M):
+    len = 0
+    lps = [0] * M
+    i = 1
+    while i < M:
+        if pat[i] == pat[len]:
+            len += 1
+            lps[i] = len
+            i += 1
+        else:
+            if len != 0:
+                len = lps[len-1]
+            else:
+                lps[i] = 0
+                i += 1
+    return lps
+
+
 res1 = naive_approach("aaba", "aabaacaadaabaaba")
 print(res1)
+print(KMP("aaacaaaaac", "aabaacaadaabaaba"))

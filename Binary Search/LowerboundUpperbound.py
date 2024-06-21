4e12ab900197de0bdef07d5d6ddacce439e2aa9c
Omkar-Shirwadkar <omi.shirwadkar@gmail.com>
2024-06-21 13:59:01 +0530

    Floor and Ceil of an array

--- Binary Search/LowerboundUpperbound.py
+++ Binary Search/LowerboundUpperbound.py
@@ -24,6 +24,36 @@ def UpperBound(arr, n, target):
             l = m + 1
     return ans
 
+#Search Insertion Problem ==> Given a sorted array insert a new element target and return its position => should be leftmost as possible
+def searchInsertionProblem(arr, n, target):
+    return LowerBound(arr, n, target)
+
+# Floor of an array is the largest number in the array which is smaller than or equal to target
+def Floor(arr, n, x):
+    l, r = 0, n - 1
+    ans = -1
+    while l <= r:
+        m = (l + r) // 2
+        if arr[m] <= x:
+            ans = arr[m]
+            l = m + 1
+        else:
+            r = m - 1
+    return ans
+
+#Ceil of an array is the smallest number in the array which is greater than or equal to target
+def Ceil(arr, n, x):
+    l, r = 0, n - 1
+    ans = -1
+    while l <= r:
+        m = (l + r) // 2
+        if arr[m] >= x:
+            ans = arr[m]
+            r = m - 1
+        else:
+            l = m + 1
+    return ans
+
 if __name__ == "__main__":
     nums = [1,2,3,3,5,8,8,10,10,11]
     n = len(nums)
@@ -33,3 +63,5 @@ if __name__ == "__main__":
             break
         print("Lowerbound index: ", LowerBound(nums, n, target))
         print("Upperbound index: ", UpperBound(nums, n, target))
+        print("Floor value: ", Floor(nums, n, target))
+        print("Ceil value: ", Ceil(nums, n, target))

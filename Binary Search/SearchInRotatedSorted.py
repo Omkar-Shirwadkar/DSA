72ad51c07f3c270770ee7303b9f1837296b0f22a
Omkar-Shirwadkar <omi.shirwadkar@gmail.com>
2024-06-22 15:30:49 +0530

    Search in rotated sorted with duplicate values

--- Binary Search/SearchInRotatedSorted.py
+++ Binary Search/SearchInRotatedSorted.py
@@ -1,14 +1,24 @@
+#Check if the array is left sorted or right sorted
 def searchInRotatedSorted(nums, n, target):
     l, r = 0, n - 1
     while l <= r:
         m = (l + r) // 2
         if nums[m] == target:
             return m
+        
+        # Edge case: when the array contains duplicate value
+        # We shrink the search space
+        # if nums[l] == nums[m] and nums[r] == nums[m]:
+        #     l += 1
+        #     r -= 1
+        #     continue
+        #Left sorted array
         elif nums[l] <= nums[m]:
             if nums[l] <= target and target <= nums[m]:
                 r = m - 1
             else:
                 l = m + 1
+        #Right sorted array
         else:
             if nums[m] <= target and target <= nums[r]:
                 l = m + 1

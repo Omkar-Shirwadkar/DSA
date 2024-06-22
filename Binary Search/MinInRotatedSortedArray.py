c2ffc698b63b59c0bc19fe73669e5c2af153f5b6
Omkar-Shirwadkar <omi.shirwadkar@gmail.com>
2024-06-22 16:07:37 +0530

    Number of times array is rotated

--- Binary Search/MinInRotatedSortedArray.py
+++ Binary Search/MinInRotatedSortedArray.py
@@ -20,7 +20,32 @@ def minimumInRotatedSortedArray(arr, n):
 
     return ans
 
+def NoOfTimesArrayIsRotated(arr, n):
+    n = len(arr)
+    l, r = 0, n - 1
+    res = -1
+    ans = float("inf")
+    while l <= r:
+        if arr[l] <= arr[r]:
+            if arr[l] < ans:
+                ans = arr[l]
+                res = l
+            break
+        m = (l + r) // 2
+        if arr[l] <= arr[m]:
+            if arr[l] < ans:
+                ans = arr[l]
+                res = l
+            l = m + 1
+        else:
+            if arr[m] < ans:
+                ans = arr[m]
+                res = m
+            r = m - 1
+    return res
+
 if __name__ == "__main__":
     arr = [1000,1023,3210,3423,5234,5439,5544,1,2,43,67,78,98,89,123,124,456,786]
     n = len(arr)
     print("Minimum value in the array is: ", minimumInRotatedSortedArray(arr, n))
+    print("Number pf times array is rotated: ", NoOfTimesArrayIsRotated(arr, n))
